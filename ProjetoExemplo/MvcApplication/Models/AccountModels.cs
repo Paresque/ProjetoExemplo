b5a9b30caeaad0e3c3fbd2324fb86063d0a26f67
Thyago Paresque <thyago.paresque@gmail.com>
2013-05-02 15:24:56 -0300

    a

--- ProjetoExemplo/MvcApplication/Models/AccountModels.cs
+++ ProjetoExemplo/MvcApplication/Models/AccountModels.cs
@@ -78,7 +78,7 @@ namespace MvcApplication.Models
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} deve be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
